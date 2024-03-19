16f02b39c85efb4b7981af64101362ca1057da19
1mpuser <alex022003@mail.ru>
2024-03-19 03:54:42 +0300

    Last fixes

--- src/components/ItemsContainer/ItemsContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './ItemsContainer.module.css';
-
-const ItemsContainer = () => {
-	return <div className={classes.container}></div>;
-};
-
-export default ItemsContainer;
